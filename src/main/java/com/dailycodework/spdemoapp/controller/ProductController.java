48841e84b0fb8b7489793987f7a975e7d28ec1f1
cristianalvarez1404 <cristianalvarez14@gmail.com>
2025-06-15 20:28:27 -0500

    new feature

--- src/main/java/com/dailycodework/spdemoapp/controller/ProductController.java
+++ src/main/java/com/dailycodework/spdemoapp/controller/ProductController.java
@@ -1,16 +1,27 @@
 package com.dailycodework.spdemoapp.controller;
 
+import com.dailycodework.spdemoapp.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private final List<Product> products = Arrays.asList(
+            new Product(1L,"Samsung",100,"Some smart phones"),
+            new Product(2L,"iPhone",500,"Some smart phones"),
+            new Product(3L,"Sneakers",150,"Some smart shoes"),
+            new Product(4L,"Dell",200,"Some smart laptops"),
+            new Product(5L,"HP",100,"Some laptop")
+    );
 
-    @GetMapping("/hello")
-    public String sayHello(@PathVariable  String name){
-        return "Hello " + name;
+    @GetMapping("/all")
+    public List<Product> getAllProducts(){
+        return products;
     }
 }
