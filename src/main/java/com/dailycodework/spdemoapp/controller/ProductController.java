03c9e4cd4901135e309fcddc2dfc68038eaced59
CristianAlvarez <cristianandres1496@gmail.com>
2025-06-18 20:51:36 -0500

    product service

--- src/main/java/com/dailycodework/spdemoapp/controller/ProductController.java
+++ src/main/java/com/dailycodework/spdemoapp/controller/ProductController.java
@@ -1,27 +1,23 @@
 package com.dailycodework.spdemoapp.controller;
 
 import com.dailycodework.spdemoapp.model.Product;
+import com.dailycodework.spdemoapp.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    private final List<Product> products = Arrays.asList(
-            new Product(1L,"Samsung",100,"Some smart phones"),
-            new Product(2L,"iPhone",500,"Some smart phones"),
-            new Product(3L,"Sneakers",150,"Some smart shoes"),
-            new Product(4L,"Dell",200,"Some smart laptops"),
-            new Product(5L,"HP",100,"Some laptop")
-    );
+    private final ProductService productService;
 
     @GetMapping("/all")
     public List<Product> getAllProducts(){
-        return products;
+        return productService.getProducts();
     }
 }
--- src/main/java/com/dailycodework/spdemoapp/service/ProductService.java
+++ src/main/java/com/dailycodework/spdemoapp/service/ProductService.java
@@ -1,4 +1,21 @@
 package com.dailycodework.spdemoapp.service;
 
+import com.dailycodework.spdemoapp.model.Product;
+import com.dailycodework.spdemoapp.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Service
 public class ProductService {
+
+
+    public List<Product> getProducts(){
+        return ProductRepository.products;
+    }
 }
